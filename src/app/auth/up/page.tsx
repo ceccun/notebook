d857ec844d26c24d3c2f62ab27189883232beaea
ejaz4 <ejazali4@protonmail.ch>
2024-10-07 17:43:37 +0100

    ✨ Wrapped in a form

--- src/app/auth/up/page.tsx
+++ src/app/auth/up/page.tsx
@@ -1,26 +1,48 @@
+"use client";
 import { ArrowRight } from "lucide-react";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 const SignUp = () => {
+	const formRef = useRef<HTMLFormElement>(null);
+
 	return (
-		<div className="modal">
-			<div className="textPair">
-				<h3>Create a Notebook account</h3>
-				<p>You need to provide details to create your Notebook account.</p>
+		<form
+			ref={formRef}
+			onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+				e.preventDefault();
+				console.log("Submit");
+			}}
+		>
+			<div className="modal">
+				<div className="textPair">
+					<h3>Create a Notebook account</h3>
+					<p>You need to provide details to create your Notebook account.</p>
+				</div>
+
+				<input type="text" name="fullname" placeholder="FULL NAME" />
+				<input type="email" name="email" placeholder="EMAIL" />
+
+				<p>
+					If you forget your password, your Notebooks will be unrecoverable.
+				</p>
+				<input
+					type="password"
+					minLength={8}
+					name="password"
+					placeholder="PASSWORD"
+				/>
+				<input
+					type="password"
+					name="passwordconfirm"
+					placeholder="CONFIRM PASSWORD"
+				/>
+
+				<button type="submit">
+					<p>Create</p>
+					<ArrowRight size={16} />
+				</button>
 			</div>
-
-			<input type="text" placeholder="FULL NAME" />
-			<input type="email" name="emaik" placeholder="EMAIL" />
-
-			<p>If you forget your password, your Notebooks will be unrecoverable.</p>
-			<input type="password" name="" placeholder="PASSWORD" />
-			<input type="password" name="" placeholder="CONFIRM PASSWORD" />
-
-			<button>
-				<p>Create</p>
-				<ArrowRight size={16} />
-			</button>
-		</div>
+		</form>
 	);
 };
 
